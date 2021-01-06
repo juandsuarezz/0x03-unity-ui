2d2fbfe3230494e93c0b8e2d369be58027af4ee4
juandsuarezz <1486@holbertonschool.com>
2021-01-06 16:16:46 -0500

    Quit game

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,15 +7,23 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     public Button playButton;
-    
+    public Button quitButton;
+
     // Start is called before the first frame update
     void Start()
     {
         playButton.onClick.AddListener(PlayMaze);
+        quitButton.onClick.AddListener(QuitMaze);
     }
 
     public void PlayMaze()
     {
         SceneManager.LoadScene("maze");
+    }
+
+    public void QuitMaze()
+    {
+        Debug.Log("Quit game");
+        Application.Quit();
     }
 }
